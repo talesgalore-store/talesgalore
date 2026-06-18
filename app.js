04ed13cb57a2a1e1c928abeac23c8fb59e806cb8
talesgalore-store <talesgalore.store@gmail.com>
2026-06-19 02:14:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,14 @@ client.getEntries({
 }).then((res) => {
   console.log(res.items);
 });
+
+<script>
+const messages = document.querySelectorAll('.announcement-message');
+let current = 0;
+setInterval(() => {
+  messages[current].classList.remove('active');
+  current = (current + 1) % messages.length;
+  messages[current].classList.add('active');
+}, 5000); // 5 seconds
+
+</script>
