d744ba682a832d4c3d416acc31de0019ae6e022a
talesgalore-store <talesgalore.store@gmail.com>
2026-06-19 01:00:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ function slide(trackId, direction) {
         behavior: 'smooth'
     });
 }
+
+const client = contentful.createClient({
+  space: "tx11zsju5n7c",
+  accessToken: "1gi_iikDoQygU8FDuM4__2GE6YWb4iJMrOYLUCsyviQ"
+});
+
+client.getEntries({
+  content_type: "literaryEntry",
+  limit: 1
+}).then((res) => {
+  console.log(res.items);
+});
